b44f7447cf6b53c49a85adb1dae28e2f5f9b3fe4
Reny Tuji Agustin <renyagustin17@icloud.com>
2023-11-14 07:01:42 -0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>tes git</title>
 </head>
 <body>
-    
+    <h1>Hello world</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque similique suscipit deserunt laudantium perspiciatis nisi, quae culpa assumenda, quasi distinctio blanditiis quos illum. Soluta iste fuga consequuntur odit, repellat architecto.</p>
 </body>
 </html>
